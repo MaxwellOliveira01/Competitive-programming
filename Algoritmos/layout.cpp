1ef542810c3ae241cc59b3bcd37c86c098eab158
Maxwell Oliveira <maxwell1de1oliveira@gmail.com>
2021-11-10 21:31:40 -0300

    k maiores

--- Algoritmos/layout.cpp
+++ Algoritmos/layout.cpp
@@ -13,18 +13,17 @@ using tii = tuple<int,int,int>;
 // auto [a,b,c] = ...
 // .insert({a,b,c})
 
-const int INF = 1e9; // INF to INT
-//const ll INF = 1e18; //INF to LL
+const int oo = (int)1e9; //INF to INT
+const ll OO = 0x3f3f3f3f3f3f3f3fLL; //INF to LL
 
 /*wa? coloca long long que passa;
 testar casos, n = 0? n = 1? todos os numeros iguais?
 Uma resposta ótima pode ter tamanho 2?
 RELER O ENUNCIADO!*/
 
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    
 
+    
 }
