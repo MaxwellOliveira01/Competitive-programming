fe617629690648d7d09a37676b81e065aca30ca3
Maxwell Oliveira <maxwell1de1oliveira@gmail.com>
2021-11-26 16:56:16 -0300

    sparse

--- Algoritmos/sparse.cpp
+++ Algoritmos/sparse.cpp
@@ -17,63 +17,50 @@ testar casos, n = 0? n = 1? todos os numeros iguais?
 Uma resposta ótima pode ter tamanho 2?
 RELER O ENUNCIADO!*/
 
-const int MAXN = 200000+1000; //valor grande tal que não precise de log de valores maiores
-const int logn = 23;
-int floor_log[MAXN+1];
-
-/*Published on
-https://github.com/MaxwellOliveira01/Competitive-programming/tree/master/Algoritmos
-*/
-
-void build_table_log(){
-    /*Chamar essa função na main antes 
-    de usar as queries da Sparse*/
-    floor_log[1] = 0;
-    for(int i = 2; i <= MAXN; i++)
-        floor_log[i] = floor_log[i/2]+1;
-}
 
 struct Sparse{
   
-    vector<vector<int>> arr;
-    
+    vector<vector<ll>> arr;
+    vector<int> floor_log;
+
     //chamar build_table_log na main
-    int op(int& a, int& b){
+    ll op(ll& a, ll& b){
         return min(a,b);
         //return max(a,b);
         //return __gcd(a,b);
     }
  
-    Sparse(vector<int>& v){ //Constrói a tabela
-        int n = v.size();
-        arr.assign(n, vector<int>(logn, 0));
-        
+    void build_table_log(int MAXN){
+        floor_log.resize(MAXN);
+        floor_log[1] = 0;
+        for(int i = 2; i < MAXN; i++)
+            floor_log[i] = floor_log[i/2]+1;
+    }
+
+    Sparse(vector<ll>& v){ //Constrói a tabela
+        int n = v.size(), logn = 0;
+        while((1<<logn) <= n) logn++;
+        build_table_log(n + 10);
+        arr.assign(n, vector<ll>(logn, 0));
         for(int i = 0; i < n; i++)
             arr[i][0] = v[i];
-
         for(int k = 1; k < logn; k++){
             for(int i = 0; i < n; i++){
                 if(i + ( 1 << k) -1 >= n)
                     break;
-                
-                int p = i+( 1 << (k-1) );
-                
+                int p = i+( 1 << (k-1) );                
                 /*fazendo o intervalo usando a linha de cima da tabela,
                 [i, 2^k-1] -> [i, 2^(k-1)-1] + [i+2^(k-1), 2^(k-1)-1  ].
                 Por exemplo, o intervalo XXXX pode ser escrito como a operação
                 em  (XXX)X e X(XXX)*. Existe intercessão.*/
-                
                 arr[i][k] = op( arr[i][ k-1 ] , arr[p][k-1]  );
             }
         }
-        
     }
 
-    int query(int l, int r){
-        
+    ll query(int l, int r){
         int pot = floor_log[r-l+1];
-        int k = (1 << pot) ;
-        
+        int k = (1 << pot) ;      
         return op(  arr[l][pot] , arr[  r - (k-1)  ][pot]   );
     }  
  
