c9c480e84637eccca1b5ad5081a100b7fe7351a2
Maxwell Oliveira <maxwell1de1oliveira@gmail.com>
2022-06-12 10:30:11 -0300

    fix seg lazy

--- segLazy.cpp
+++ segLazy.cpp
@@ -27,7 +27,7 @@ struct SegLazy {
     vector<ll> seg;
     vector<ll> lazy;
 
-    SegLazy(vector<int>& arr){
+    SegLazy(vector<ll>& arr){
         n = (int)arr.size();
         seg.assign(n+n+n+n, 0);
         lazy.assign(n+n+n+n, 0);
@@ -38,9 +38,9 @@ struct SegLazy {
         return a+b;
     }
 
-    void build(ll x, int l, int r, vector<int>& arr){
+    void build(ll x, int l, int r, vector<ll>& arr){
         if(l == r){
-            seg[x] = arr[l];
+            seg[x] = 1LL * arr[l];
         } else {
             int mid = l + (r-l)/2;
             build(x+x, l, mid, arr);
@@ -50,7 +50,6 @@ struct SegLazy {
     }
     
     void upd_lazy(ll node, ll l, ll r){
-        if(!lazy[node]) return;
         seg[node] += (ll)(r-l+1) * lazy[node];
         ll esq = node + node, dir = esq + 1;
         
@@ -64,14 +63,18 @@ struct SegLazy {
     
     ll q(ll x, int l, int r, int i, int j){
         upd_lazy(x,l,r);
-        if(r < i || l > j ) return 0;
-        if(l >= i && r <= j ) return seg[x];
+        
+        if(r < i || l > j) 
+            return 0;
+        
+        if(l >= i && r <= j ) 
+            return seg[x];
+        
         int mid = l + (r-l)/2;
-        upd_lazy(x,l,r);
         return merge(q(x+x,l,mid,i,j), q(x+x+1,mid+1,r,i,j));
     }
     
-    ll query(int l, int r){ //valor em uma posi específica -> query de [l,l]
+    ll query(int l, int r){ //valor em uma posi específica -> query de [l,l];
         return q(1, 0, n-1, l, r);
     }
     
@@ -79,11 +82,10 @@ struct SegLazy {
         upd_lazy(x,l,r);
         if(r < i || l > j) return;
         if(l >= i && r <= j){
-            upd_lazy(x,l,r);
             lazy[x] += u;
+            upd_lazy(x,l,r);
         } else {
             int mid = l + (r-l)/2;
-            upd_lazy(x,l,r);
             upd(x+x,l,mid,i,j,u);
             upd(x+x+1,mid+1,r,i,j,u);
             seg[x] = merge(seg[x+x], seg[x+x+1]);
@@ -95,6 +97,7 @@ struct SegLazy {
     }
 
 };
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
